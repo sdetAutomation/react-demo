397b3c8b2f4ac5d8928935d0f993ce02cf891e30
KinWong <kin.wong@target.com>
2019-01-20 13:31:40 -0600

    Add components

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+// This component handles the App template used on every page.
+import React, {PropTypes} from 'react'
+import Header from './common/Header'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Header /> 
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+}
+
+export default App
+// function mapStateToProps(state, ownProps) {
+//   return {
+//     loading: state.ajaxCallsInProgress > 0
+//   }
+// }
+
+// export default connect(mapStateToProps)(App)
