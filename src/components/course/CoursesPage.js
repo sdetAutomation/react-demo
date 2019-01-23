faa371ab60edd8f60c3000788d9f910a967da84c
KinWong <kin.wong@target.com>
2019-01-23 14:05:04 -0600

    Completed redux flow, and added contants

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,6 +1,8 @@
 import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
 import * as courseActions from '../../actions/courseActions'
+import { bind } from 'bluebird';
 
 
 class CoursesPage extends React.Component {
@@ -22,7 +24,7 @@ class CoursesPage extends React.Component {
     }
 
     onClickSave() {
-        this.props.dispatch(courseActions.createCourse(this.state.course))
+        this.props.actions.createCourse(this.state.course)
     }
 
     courseRow(course, index) {
@@ -50,8 +52,8 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
-    dispatch: PropTypes.func.isRequired,
-    courses: PropTypes.array.isRequired
+    courses: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
 }
 
 function mapStateToProps(state, ownProps) {
@@ -60,4 +62,10 @@ function mapStateToProps(state, ownProps) {
     }
 }
 
-export default connect(mapStateToProps)(CoursesPage)
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(courseActions, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage)
